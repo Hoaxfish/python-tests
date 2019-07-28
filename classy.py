27d469bbc401738b593fbef4f30d56be133695ec
Hoaxfish <46001663+Hoaxfish@users.noreply.github.com>
2019-07-28 03:18:05 +0100

    class example

---
+++ classy.py
@@ -0,0 +1,17 @@
+class makeIt ():
+    dogs = "here"
+
+    def once(self, parameter_list):
+        self.house = parameter_list
+        pass
+
+    def twice():
+        print(self)
+        pass
+
+c = makeIt()
+d = makeIt()
+d.once("horse")
+c.once("houses")
+
+print(c.house, d.house)
