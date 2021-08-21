3fd348dd006607338ec0720bea7d9ec2cb31cb48
Hoaxfish <46001663+Hoaxfish@users.noreply.github.com>
2021-08-21 21:33:40 +0100

    arg test

---
+++ sub.py
@@ -0,0 +1,7 @@
+import sys
+
+arguments = sys.argv
+print("Items in the list 'arguments'")
+print("Args: \t\t\t" + str(arguments))
+print("Script name: " + arguments[0])
+print("First Arg: " + arguments[1])
